c488ec7862baec26579848f26a87215a3c471cfa
saeyeongp <saeyeong@weversecompany.com>
2022-03-02 21:31:15 +0900

    [LET-15]Android 12버전 대응한 Splash screen 추가

---
+++ app/src/main/java/com/github/caterpie/letsgitit/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.caterpie.letsgitit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		val splashScreen = installSplashScreen()
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
